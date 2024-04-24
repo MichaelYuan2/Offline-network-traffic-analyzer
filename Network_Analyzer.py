e3c0e18f67aa3c49e6bb0173b5d209e83662ae63
MichaelYuan2 <hy2203@nyu.edu>
2024-04-24 12:11:53 +0800

    update Netwrok_Analyzer

--- Network_Analyzer.py
+++ Network_Analyzer.py
@@ -1,14 +1,3 @@
-# import all class
-from Extractor.DHCP import DHCP
-from Extractor.DNS import DNS
-from Extractor.HTTP import HTTP
-from Extractor.TCP import TCP
-from Extractor.UDP import UDP
-from Extractor.IP import IP
-
-# load text hexdump
-with open('hexdump.txt', 'r') as file:
-    hexdump = file.read()
 # modify the hexdump to remove the offset
 
 # EtherNet Header
